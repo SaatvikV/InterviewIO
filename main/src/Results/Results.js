0927f9844a93e77a73857550008683e53a015dd1
Steven Zhang <50186662+sbeven@users.noreply.github.com>
2023-04-08 19:57:01 -0400

    added chat history

--- main/src/Results/Results.js
+++ main/src/Results/Results.js
@@ -5,20 +5,31 @@ import "./Results.css";
 class Results extends Component {
 
   render() {
-         if (sessionStorage.getItem("history")== null){
-           sessionStorage.setItem("history", [])
+    const num_convos = sessionStorage.getItem("num_items");
+    var hist = [];
+    for (var i = 1; i <= num_convos; i++) {
+        const gdate = sessionStorage.getItem("item"+ i + "date");
+        const gmonth = sessionStorage.getItem("item"+ i + "month");
+        const gyear = sessionStorage.getItem("item"+ i + "year");
+        const gtime = sessionStorage.getItem("item"+ i + "time");
+        const glength = sessionStorage.getItem("item" + i + "length");
+        var msgs = [];
+        for (var j = 0; j < glength; j++) {
+            msgs.push(sessionStorage.getItem("item" + i + "msg" + j));
         }
+        hist.push({date: (parseInt(gmonth) + 1) + "/" + gdate + "/" +gyear, time: gtime, length: glength, msg: msgs});
+    }
     return (
 
       <div className="Results">
         <div className="lander">
           <h1>Interview History</h1>
            <ul>
-      {sessionStorage.getItem("history").map((data) => (
-        <li>
-          <p>{data.date}</p>
-          <p>{data.time}</p>
-          <p>{data.msg}</p>
+      {hist.map((data) => (
+        <li key={data.time}>
+          <p>Date: {data.date}</p>
+          <p>Time: {data.time}</p>
+          <p>Conversation Length: {data.length}</p>
         </li>
       ))}
     </ul>
@@ -29,13 +40,33 @@ class Results extends Component {
 
 
     static buttonHandler(convo) {
-    let currentDate = new Date();
-    let datetemp = currentDate.getDate();
-    let timetemp = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
-        if (sessionStorage.getItem("history")== null){
-           sessionStorage.setItem("history", [])
-        }
-        sessionStorage.setItem("history", sessionStorage.getItem("history").concat({date: datetemp, time: timetemp, msg: convo}));
+    const currentDate = new Date();
+    const datetemp = currentDate.getDate();
+    const monthtemp = currentDate.getMonth();
+    const yeartemp = currentDate.getFullYear();
+    const timetemp = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+    var num = sessionStorage.getItem("num_items");
+    if (num == null)   {
+        sessionStorage.setItem("num_items", "0");
+        num = "0";
     }
+
+    var num = parseInt(num) + 1;
+    const numString = num.toString();
+    sessionStorage.setItem("num_items", num);
+    sessionStorage.setItem("item" + numString + "date", datetemp);
+    sessionStorage.setItem("item" + numString + "month", monthtemp);
+    sessionStorage.setItem("item" + numString + "year", yeartemp);
+    sessionStorage.setItem("item" + numString + "time", timetemp);
+    sessionStorage.setItem("item" + numString + "length", convo.length);
+    for (var i = 0; i < convo.length; i++) {
+        sessionStorage.setItem("item"+ numString + "msg" + i, convo[i]);
+    }
+
+
+
+
+    }
+
 }
 export default Results;
