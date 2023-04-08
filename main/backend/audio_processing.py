ee4f5e4ab0486d22bf6a2bde0a06dafb37c851b7
Adam Yang <66138604+adamy03@users.noreply.github.com>
2023-04-08 13:15:42 -0400

    updated prompt;;

--- main/backend/audio_processing.py
+++ main/backend/audio_processing.py
@@ -8,11 +8,14 @@ import openai
 # Set API key
 openai.api_key = key.KEY
 
+PROMPT = 'This transcription is of an interviewee responding to a question about their background. The prompt they were given was: '
+
 def speech_to_text(file: str) -> str:
     """
     Reads in a file then tries to convert to text using openai call. File uploads are currently 
     limited to 25 MB and the following input file types are supported:
     mp3, mp4, mpeg, mpga, m4a, wav, and webm.
+    This method return text. 
     """
     audio_file = open(file, 'rb')
     try:
@@ -25,9 +28,9 @@ def transcribe(file: str, prompt: str) -> str:
     Prompt informed transcription of audio file.
     """
     audio_file = open(file, 'rb')
-    prompt = 'This transcription is of an interviewee responding to a question about their background. The prompt they were given was: ' + prompt
+    transcription_prompt = PROMPT + prompt
     
     try:
-        return openai.Audio.transcribe('whisper-1', audio_file, prompt=prompt)['text']
+        return openai.Audio.transcribe('whisper-1', audio_file, prompt=transcription_prompt)
     except UnicodeDecodeError:
         raise Exception('could not read file')
--- main/backend/base.py
+++ main/backend/base.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, request, redirect, jsonify
+from audio_processing import *
 
 api = Flask(__name__)
 
-@api.route()
-def my_profile():
-    response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
+@api.route('/audio', methods=['GET', 'POST'])
+def get_audio():
+    if request.method == 'POST':
+        audio = request.args.get('audio') #get audio from front end
+        prompt = request.args.get('prompt') #get prompt 
+        
+        transcribe(file=audio, prompt=prompt)
+        
+        return transcribe
 
-    return response_body
 
